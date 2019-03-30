85ed7828b3b1e2da78f5a9ef626e335fdf18a5f8
Sem Schilder <sschilder@transip.nl>
2019-03-30 12:56:57 +0100

    Codestyle macht frei 👨🏻‍🎨

--- cron.php
+++ cron.php
@@ -3,15 +3,15 @@ require('common.php');
 
 $time = file_get_contents(FILENAME_TIME);
 if ($time === false) {
-	die();
+    die();
 }
 $time = trim($time);
 
 if (time() > $time) {
-	$chatId = trim(file_get_contents(FILENAME_CHATID));
-	
-	sendMessage($chatId, QUOTES[array_rand (QUOTES)] . PHP_EOL . implode (' ', $members));
+    $chatId = trim(file_get_contents(FILENAME_CHATID));
+    
+    sendMessage($chatId, QUOTES[array_rand(QUOTES)] . PHP_EOL . implode(' ', $members));
 
-	unlink(FILENAME_TIME);
-	unlink(FILENAME_CHATID);
+    unlink(FILENAME_TIME);
+    unlink(FILENAME_CHATID);
 }
