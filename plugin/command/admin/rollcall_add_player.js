c6a348d11b89dcab826a4f2b41dda43a95853a7e
Robin Jacobs <robin@robinj.be>
2026-01-08 16:50:11 +0000

    Support text_mention entities in rollcall commands.

--- plugin/command/admin/rollcall_add_player.js
+++ plugin/command/admin/rollcall_add_player.js
@@ -5,7 +5,7 @@ const dao = new DAO();
 
 module.exports = (api, message) => {
     const args = message.meta.command?.argumentTokens;
-    const mentions = message.message.entities.filter(entity => entity?.type === 'mention');
+    const mentions = (message.message.entities || []).filter(entity => entity?.type === 'mention' || entity?.type === 'text_mention');
 
     if (args.length === 0) {
         message.reply('Who would you like to add?');
@@ -18,13 +18,21 @@ module.exports = (api, message) => {
         return;
     }
 
+    const players = args.map((arg, i) => {
+        const mention = mentions[i];
+        if (mention?.type === 'text_mention') {
+            return `[${arg}](tg://user?id=${mention.user.id})`;
+        }
+        return arg;
+    });
+
     dao.getRollcallPlayerUsernames(message.message.chat.id)
-        .then(existingPlayers => existingPlayers.forEach(existingPlayer => {
-            if (args.includes(existingPlayer)) {
-                throw new Error(`Player ${existingPlayer} is already in the rollcall.`);
+        .then(existingPlayers => players.forEach(player => {
+            if (existingPlayers.includes(player)) {
+                throw new Error(`Player ${player} is already in the rollcall.`);
             }
         }))
-        .then(() => Promise.all(args.map(player => dao.addRollcallPlayer(message.message.chat.id, player))))
+        .then(() => Promise.all(players.map(player => dao.addRollcallPlayer(message.message.chat.id, player))))
         .then(() => message.reply(`Added ${args.length} players to rollcall.`))
         .catch(err => message.reply(formatError(err)));
 }
