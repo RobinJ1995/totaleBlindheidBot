7646a354d6be1afdbe2813837636b76d610691d4
Robin Jacobs <robin@robinj.be>
2026-01-19 21:33:07 +0100

    remove dist/

--- dist/command/hi.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = (bot, msg) => {
-    const who = msg.from?.first_name || msg.from?.username || 'stranger';
-    msg.reply(`Hello to you too, ${who}!`);
-};
